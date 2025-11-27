68313296cf1a1855b811c76837dd9f247f981dcc
steven <stevenraison@outlook.fr>
2025-11-27 12:18:14 +0100

    add register page and fix navbar

--- classroom-frontend/src/app/app.routes.ts
+++ classroom-frontend/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { DashboardAd } from './pages/dashboard-ad/dashboard-ad';
 import { Accueil } from './pages/accueil/accueil';
-import path from 'path';
+import { Inscription } from './pages/inscription/inscription';
+
 export const routes: Routes = [
 
    {path: '', component: Accueil},
    { path: 'GestionAdmin', component: DashboardAd },
+   {path:'inscription',component:Inscription}
 ];
